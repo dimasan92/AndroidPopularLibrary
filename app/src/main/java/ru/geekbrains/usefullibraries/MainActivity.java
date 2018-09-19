815e3670a987cea612452546fcf06778c4f56f35
dimasan92 <dimasan92@gmail.com>
2018-09-19 23:43:40 +0300

    editText and textView was bound

--- app/src/main/java/ru/geekbrains/usefullibraries/MainActivity.java
+++ app/src/main/java/ru/geekbrains/usefullibraries/MainActivity.java
@@ -2,16 +2,20 @@ package ru.geekbrains.usefullibraries;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.arellomobile.mvp.MvpAppCompatActivity;
 import com.arellomobile.mvp.presenter.InjectPresenter;
 import com.arellomobile.mvp.presenter.ProvidePresenter;
+import com.jakewharton.rxbinding2.widget.RxTextView;
 
 import java.util.Locale;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import io.reactivex.disposables.Disposable;
 
 public class MainActivity extends MvpAppCompatActivity implements MainView {
 
@@ -21,15 +25,34 @@ public class MainActivity extends MvpAppCompatActivity implements MainView {
     Button buttonTwo;
     @BindView(R.id.btn_three)
     Button buttonThree;
+    @BindView(R.id.tv_bind)
+    TextView bindTextView;
+    @BindView(R.id.et_bind)
+    EditText bindEditText;
 
     @InjectPresenter
     MainPresenter presenter;
+
+    private Disposable editTextDisposable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        editTextDisposable = RxTextView.textChanges(bindEditText)
+                .subscribe(bindTextView::setText);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        editTextDisposable.dispose();
     }
 
     @ProvidePresenter
