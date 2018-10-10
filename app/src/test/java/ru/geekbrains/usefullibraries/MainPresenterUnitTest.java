d794d5de0016547141e80d5c36c397ec4ad5d80b
dimasan92 <dimasan92@gmail.com>
2018-10-10 15:20:12 +0300

    load info success test

--- app/src/test/java/ru/geekbrains/usefullibraries/MainPresenterUnitTest.java
+++ app/src/test/java/ru/geekbrains/usefullibraries/MainPresenterUnitTest.java
@@ -10,7 +10,16 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Single;
 import io.reactivex.schedulers.TestScheduler;
+import ru.geekbrains.usefullibraries.di.DaggerTestComponent;
+import ru.geekbrains.usefullibraries.di.TestComponent;
+import ru.geekbrains.usefullibraries.di.modules.TestRepoModule;
+import ru.geekbrains.usefullibraries.mvp.model.api.UserRepo;
+import ru.geekbrains.usefullibraries.mvp.model.entity.User;
 import ru.geekbrains.usefullibraries.mvp.presenter.MainPresenter;
 import ru.geekbrains.usefullibraries.mvp.view.MainView;
 
@@ -42,6 +51,28 @@ public class MainPresenterUnitTest {
     }
 
     @Test
-    public void loadInfoCall() {
+    public void loadInfoSuccessTest() {
+        User user = new User("googlesamples", "avatarUrl", "reposUrl");
+        TestComponent testComponent = DaggerTestComponent.builder()
+                .testRepoModule(new TestRepoModule() {
+                    @Override
+                    public UserRepo provideUserRepo() {
+                        UserRepo userRepo = super.provideUserRepo();
+                        Mockito.when(userRepo.getUser("googlesamples")).thenReturn(Single.just(user));
+                        Mockito.when(userRepo.getUserRepos(user)).thenReturn(Single.just(new ArrayList<>()));
+                        return userRepo;
+                    }
+                })
+                .build();
+
+        testComponent.inject(presenter);
+        presenter.attachView(mainView);
+        presenter.onPermissionsGranted();
+        testScheduler.advanceTimeBy(1, TimeUnit.SECONDS);
+
+        Mockito.verify(mainView).hideLoading();
+        Mockito.verify(mainView).setUsername(user.getLogin());
+        Mockito.verify(mainView).showAvatar(user.getAvatarUrl());
+        Mockito.verify(mainView).updateRepoList();
     }
 }
