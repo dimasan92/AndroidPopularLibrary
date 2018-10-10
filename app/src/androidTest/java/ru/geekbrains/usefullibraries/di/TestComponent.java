df0e0b044f71dba666f51f24a2af24af03f19709
dimasan92 <dimasan92@gmail.com>
2018-10-10 22:46:47 +0300

    provide mock files utils

--- app/src/androidTest/java/ru/geekbrains/usefullibraries/di/TestComponent.java
+++ app/src/androidTest/java/ru/geekbrains/usefullibraries/di/TestComponent.java
@@ -3,14 +3,16 @@ package ru.geekbrains.usefullibraries.di;
 import javax.inject.Singleton;
 
 import dagger.Component;
+import ru.geekbrains.usefullibraries.PaperImageCacheInstrumentedTest;
 import ru.geekbrains.usefullibraries.PaperUserCacheInstrumentedTest;
 import ru.geekbrains.usefullibraries.RealmUserCacheInstrumentedTest;
 import ru.geekbrains.usefullibraries.UserRepoInstrumentedTest;
+import ru.geekbrains.usefullibraries.di.modules.TestUtilsModule;
 import ru.geekbrains.usefullibraries.mvp.di.modules.CacheModule;
 import ru.geekbrains.usefullibraries.mvp.di.modules.RepoModule;
 
 @Singleton
-@Component(modules = {RepoModule.class, CacheModule.class})
+@Component(modules = {RepoModule.class, CacheModule.class, TestUtilsModule.class})
 public interface TestComponent {
 
     void inject(UserRepoInstrumentedTest test);
@@ -18,4 +20,6 @@ public interface TestComponent {
     void inject(PaperUserCacheInstrumentedTest test);
 
     void inject(RealmUserCacheInstrumentedTest test);
+
+    void inject(PaperImageCacheInstrumentedTest test);
 }
